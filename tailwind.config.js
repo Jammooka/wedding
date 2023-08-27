b9dcf8a0c8ada98db49bd4a6478cef89485ce620
Jamie Blacker <jamie@comparemymove.com>
2023-08-27 21:42:28 +0100

    get fonts up and running

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['caflisch-script-pro', 'ui-sans-serif'],
+      'serif': ['Old Standard TT', 'ui-serif'],
+    },
+    screens: {
+      'xs': '560px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        wlgreen: '#053330',
+        wlgreenlight: '#074a46',
+        wlgold: '#9f8223',
+        white: 'white'
+      },
+    }
   },
   plugins: [],
 }
